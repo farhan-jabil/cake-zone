ce9a3c5d4d8ee006a9ba470e061decbeb05d7918
farhan-jabil <farhanhasan295@gmail.com>
2024-06-03 15:27:49 +0600

    bottom navbar done with responsive

--- src/components/Navbar/NavbarBottom.js
+++ src/components/Navbar/NavbarBottom.js
@@ -1,31 +1,38 @@
-import React, { useState } from 'react';
-import { Link, useLocation } from 'react-router-dom';
+import React, { useState } from "react";
+import { Link, useLocation } from "react-router-dom";
 
 const NavbarBottom = () => {
   const location = useLocation();
   const currentPath = location.pathname;
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+  const [isMobileDropdownOpen, setIsMobileDropdownOpen] = useState(false);
 
   const getLinkClass = (path) =>
     `block rounded transition duration-500 ease-in-out ${
       currentPath === path
         ? "text-[#E88F2F]"
         : "text-white hover:text-[#E88F2F]"
-    } hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:p-0 dark:text-white dark:hover:bg-gray-700 md:dark:hover:bg-transparent`;
+    } md:border-0 md:p-0 dark:text-white dark:hover:bg-gray-700 md:dark:hover:bg-transparent`;
 
   const toggleDropdown = () => {
     setIsDropdownOpen(!isDropdownOpen);
   };
 
+  const toggleMobileMenu = () => {
+    setIsMobileMenuOpen(!isMobileMenuOpen);
+  };
+
+  const toggleMobileDropdown = () => {
+    setIsMobileDropdownOpen(!isMobileDropdownOpen);
+  };
+
   return (
     <>
       <div>
-        <nav className="bg-[#2B2825] border-gray-200 dark:bg-gray-900">
+        <nav className="hidden md:block bg-[#2B2825] border-gray-200 dark:bg-gray-900">
           <div className="max-w-screen-xl flex items-center mx-auto">
-            <div
-              className="hidden mx-auto w-full md:block md:w-auto"
-              id="navbar-default"
-            >
+            <div className="mx-auto w-full md:w-auto" id="navbar-default">
               <ul className="font-medium text-lg flex flex-col mt-4 border border-gray-100 rounded-lg md:flex-row rtl:space-x-reverse md:mt-0 md:border-0 dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
                 <li className="py-8 px-4 cursor-pointer">
                   <Link to="/" className={getLinkClass("/")}>
@@ -63,19 +70,7 @@ const NavbarBottom = () => {
                     }`}
                   >
                     <span>PAGES</span>
-                    <svg
-                      className={`w-4 h-4 ml-1 transition-transform duration-300 ${
-                        isDropdownOpen ? "rotate-180" : ""
-                      }`}
-                      fill="currentColor"
-                      viewBox="0 0 20 20"
-                    >
-                      <path
-                        fillRule="evenodd"
-                        d="M5.23 7.21a.75.75 0 011.06 0L10 10.44l3.71-3.23a.75.75 0 011.06 1.06l-4 3.5a.75.75 0 01-1.06 0l-4-3.5a.75.75 0 010-1.06z"
-                        clipRule="evenodd"
-                      />
-                    </svg>
+                    <i className="fa-solid fa-caret-down ml-[6px]"></i>
                   </div>
                   {isDropdownOpen && (
                     <ul className="absolute left-0 mt-2 w-48 bg-white rounded-md shadow-lg dark:bg-gray-800">
@@ -101,15 +96,124 @@ const NavbarBottom = () => {
                   )}
                 </li>
                 <li className="py-8 px-4 cursor-pointer">
-                  <Link
-                    to="/contact"
-                    className={getLinkClass("/contact")}
-                  >
+                  <Link to="/contact" className={getLinkClass("/contact")}>
                     CONTACT US
                   </Link>
                 </li>
               </ul>
             </div>
+          </div>
+        </nav>
+        <nav className="block md:hidden bg-[#2B2825] border-b-[1px] border-gray-200 dark:bg-gray-900 p-4">
+          <div className="flex justify-between items-center">
+            <Link to="/">
+              <div className="flex h-full text-[26px] space-x-3 justify-center items-center">
+                <div>
+                  <i className="fa-solid fa-cake-candles text-[#E88F2F]"></i>
+                </div>
+                <div className="font-bold text-white">CAKEZONE</div>
+              </div>
+            </Link>
+            <div
+              className="py-1 px-4 border-[1px] border-[#9F9E9D] cursor-pointer"
+              onClick={toggleMobileMenu}
+            >
+              <i className="fa-solid fa-bars text-[#9F9E9D] text-xl"></i>
+            </div>
+          </div>
+          <div
+            className={`${
+              isMobileMenuOpen ? "max-h-screen" : "max-h-0"
+            } overflow-hidden transition-max-height duration-500 ease-in-out`}
+          >
+            <ul className="font-medium text-lg flex flex-col mt-4 dark:bg-gray-800 dark:border-gray-700">
+              <li className="py-4 px-4 cursor-pointer">
+                <Link
+                  to="/"
+                  className={getLinkClass("/")}
+                  onClick={toggleMobileMenu}
+                >
+                  HOME
+                </Link>
+              </li>
+              <li className="py-4 px-4 cursor-pointer">
+                <Link
+                  to="/about"
+                  className={getLinkClass("/about")}
+                  onClick={toggleMobileMenu}
+                >
+                  ABOUT US
+                </Link>
+              </li>
+              <li className="py-4 px-4 cursor-pointer">
+                <Link
+                  to="/menu-pricing"
+                  className={getLinkClass("/menu-pricing")}
+                  onClick={toggleMobileMenu}
+                >
+                  MENU & PRICING
+                </Link>
+              </li>
+              <li className="py-4 px-4 cursor-pointer">
+                <Link
+                  to="/master-pages"
+                  className={getLinkClass("/master-pages")}
+                  onClick={toggleMobileMenu}
+                >
+                  MASTER PAGES
+                </Link>
+              </li>
+              <li className="relative py-4 px-4 cursor-pointer">
+                <div
+                  onClick={toggleMobileDropdown}
+                  className={`flex items-center cursor-pointer ${
+                    currentPath.startsWith("/pages")
+                      ? "text-[#E88F2F]"
+                      : "text-white hover:text-[#E88F2F]"
+                  }`}
+                >
+                  <span>PAGES</span>
+                  <i className="fa-solid fa-caret-down ml-[10px]"></i>
+                </div>
+                {isMobileDropdownOpen && (
+                  <ul className="mt-2 pl-4 border-l-2 border-gray-700">
+                    <li className="py-2">
+                      <Link
+                        to="/service"
+                        className="block text-white dark:text-gray-200 dark:hover:bg-gray-600"
+                        onClick={() => {
+                          setIsMobileDropdownOpen(false);
+                          setIsMobileMenuOpen(false);
+                        }}
+                      >
+                        Our Service
+                      </Link>
+                    </li>
+                    <li className="py-2">
+                      <Link
+                        to="/testimonial"
+                        className="block text-white dark:text-gray-200 dark:hover:bg-gray-600"
+                        onClick={() => {
+                          setIsMobileDropdownOpen(false);
+                          setIsMobileMenuOpen(false);
+                        }}
+                      >
+                        Testimonial
+                      </Link>
+                    </li>
+                  </ul>
+                )}
+              </li>
+              <li className="py-4 px-4 cursor-pointer">
+                <Link
+                  to="/contact"
+                  className={getLinkClass("/contact")}
+                  onClick={toggleMobileMenu}
+                >
+                  CONTACT US
+                </Link>
+              </li>
+            </ul>
           </div>
         </nav>
       </div>
