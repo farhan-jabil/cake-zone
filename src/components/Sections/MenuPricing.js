d72b39bc7d0c2e24a66063f6b9fc74cdcdb40425
farhan-jabil <farhanhasan295@gmail.com>
2024-06-12 23:06:02 +0600

    working on menu-pricing

--- src/components/Sections/MenuPricing.js
+++ src/components/Sections/MenuPricing.js
@@ -1,13 +1,22 @@
-import React from 'react'
+import React from "react";
 
 const MenuPricing = () => {
   return (
     <>
-    <div className='menu-pricing py-96'>
-      Menu & Pricing
-    </div>
+      <div className="menu-pricing pt-[3rem]">
+        <div className="container">
+          <div className="section-title relative mb-[3rem]">
+            <div className="text-center text-2xl lg:text-[2rem] font-bold text-[#E88F2F] pacifico-family">
+              Menu & Pricing
+            </div>
+            <div className="text-[40px] lg:text-[3.5rem] font-bold text-center pb-4">
+              EXPLORE OUR CAKES
+            </div>
+          </div>
+        </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default MenuPricing
+export default MenuPricing;
